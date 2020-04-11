361c9da219077b899d414b4f6600c1e708d7911a
upieez <samuelhww@gmail.com>
2020-04-11 20:50:24 +0800

    encoding HTML for php when you echo (important)

--- private/functions.php
+++ private/functions.php
@@ -16,4 +16,8 @@ function raw_u($string=""){
     return rawurlencode($string);
 }
 
+function h($string=""){
+    return htmlspecialchars($string);
+}
+
 ?>
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -7,10 +7,10 @@ $position = $_GET['position'] ?? '1'; // PHP > 7.0
 $visible = isset($_GET['visible']) ? $_GET['id'] : '1'; // PHP < 7.0
 $name = $_GET['name'] ?? 'GBI'; // PHP > 7.0
 
-echo $id . "<br />";
-echo $position . "<br />";
-echo $visible . "<br />";
-echo $name . "<br />";
+echo h($id) . "<br />";
+echo h($position) . "<br />";
+echo h($visible) . "<br />";
+echo h($name) . "<br />";
 
 ?>
 
