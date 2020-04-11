071ff0bc0fd32992edae146ad0fb0e400e935a5e
upieez <samuelhww@gmail.com>
2020-04-11 18:29:11 +0800

    add styling to staff index

---
+++ public/staff/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>GBI Staff Area</h1>
+    </header>
+
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+
+    <div id="content">
+    </div>
+
+    <footer>
+        &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
+</body>
+</html>
