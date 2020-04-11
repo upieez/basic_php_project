ea55fdc39698d9969f6e893696efd2fee73f3565
upieez <samuelhww@gmail.com>
2020-04-11 20:18:19 +0800

    trying to pass parameters around

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,7 +1,11 @@
 <?php
 
 $id = $_GET['id'];
+$position = $_GET['position'];
+$visible = $_GET['visible'];
 
-echo $id;
+echo $id . "<br />";
+echo $position . "<br />";
+echo $visible . "<br />";
 
 ?>
