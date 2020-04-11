c10c59b2747e73ad2c68b8337ea0c6ddfd1b5a31
upieez <samuelhww@gmail.com>
2020-04-11 20:41:45 +0800

    encoding URL parameters

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,8 +1,8 @@
+<?php require_once('../../../private/initialize.php'); ?>
 <?php
 
 
-
-$id = $_GET['id'] ?? '1'; // PHP > 7.0
+$id = $_GET['id'] ?? '1'; // PHP > 7.0 Default value if it does not exists
 $position = $_GET['position'] ?? '1'; // PHP > 7.0
 $visible = isset($_GET['visible']) ? $_GET['id'] : '1'; // PHP < 7.0
 $name = $_GET['name'] ?? 'GBI'; // PHP > 7.0
@@ -13,3 +13,7 @@ echo $visible . "<br />";
 echo $name . "<br />";
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Links</a><br />
+<a href="show.php?name=<?php echo u('Widgets&More'); ?>">Links</a><br />
+<a href="show.php?name=<?php echo u('!#*?'); ?>">Links</a><br />
