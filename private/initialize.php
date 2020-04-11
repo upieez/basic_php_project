0897c1a12de3a432ff28a862970851213d1f454a
upieez <samuelhww@gmail.com>
2020-04-11 19:57:50 +0800

    add dynamic links and urls

--- private/initialize.php
+++ private/initialize.php
@@ -10,6 +10,20 @@
     define('PUBLIC_PATH', PROJECT_PATH . '/public');
     define('SHARED_PATH', PRIVATE_PATH . '/shared');
 
+    /*------------------------------------------------------ 
+        Assign the root URL to a PHP constant
+        * Do not need to include the domain
+        * Use same document root as webserver
+        * Can set a hardcoded value:
+        define("WWW_ROOT", '/basic_php_project/public'); 
+        define("WWW_ROOT", ''); -- production mode
+        * Can dynamically find everything in URL up to "/public"
+    --------------------------------------------------------*/
+    $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7; // development mode
+    $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+    define("WWW_ROOT", $doc_root);
+
+
     require_once('functions.php');
 
 ?>
