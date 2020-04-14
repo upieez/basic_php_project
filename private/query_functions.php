4cca3fba1e5bcdba6fa20e075e60760eab022f85
upieez <samuelhww@gmail.com>
2020-04-15 00:48:09 +0800

    CRUD with PHP
    Done till create

--- private/query_functions.php
+++ private/query_functions.php
@@ -11,6 +11,68 @@
         return $result;
     }
 
+    function find_subject_by_id($id){
+        global $db;
+
+        $sql = "SELECT * FROM subjects ";
+        $sql .= "WHERE id='" . $id . "'";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        $subject = mysqli_fetch_assoc($result); 
+        // since i'm done with this data, i can free up the data set
+        mysqli_free_result($result);
+        return $subject; // returns an assoc array
+    }
+
+    function update_subject($subject){
+        global $db;
+
+        $sql = "UPDATE subjects SET ";
+        $sql .= "menu_name='" . $subject['menu_name'] . "',";
+        $sql .= "position='" . $subject['position'] . "',";
+        $sql .= "visible='" . $subject['visible'] . "' ";
+        $sql .= "WHERE id='" . $subject['id'] . "' ";
+        $sql .= "LIMIT 1"; // not necessary but it's extra safety measure
+
+        $result = mysqli_query($db, $sql);
+        // For UPDATE statements, $result is true/false
+        if($result){
+            // redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
+            return true;
+        } else {
+            // UPDATE failed
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit;
+        }
+    }
+
+    function insert_subject($subject) {
+        global $db;
+
+        $menu_name = $_POST['menu_name'] ?? '';
+        $position = $_POST['position'] ?? '';
+        $visible = $_POST['visible'] ?? '';
+        
+        $sql =  "INSERT INTO subjects ";
+        $sql .= "(menu_name, position, visible) ";
+        $sql .= "VALUES (";
+        $sql .= "'" . $subject['menu_name'] . "',";
+        $sql .= "'". $subject['position'] . "',";
+        $sql .= "'" . $subject['visible'] . "'";
+        $sql .= ")";
+        $result = mysqli_query($db,$sql);
+        // For INSERT statements, $result is true/false
+        if($result){
+            return true;
+        } else {
+            // INSERT failed report back the error
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit; // quit everything and not run
+        }
+    }
+
     function find_all_pages(){
         global $db;
 
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,19 +1,42 @@
 <?php require_once('../../../private/initialize.php'); ?>
+
 <?php
+// $id = isset($_GET['id']) ? $_GET['id'] : '1';
+$id = $_GET['id'] ?? '1'; // PHP > 7.0
 
-
-$id = $_GET['id'] ?? '1'; // PHP > 7.0 Default value if it does not exists
-$position = $_GET['position'] ?? '1'; // PHP > 7.0
-$visible = isset($_GET['visible']) ? $_GET['id'] : '1'; // PHP < 7.0
-$name = $_GET['name'] ?? 'GBI'; // PHP > 7.0
-
-echo h($id) . "<br />";
-echo h($position) . "<br />";
-echo h($visible) . "<br />";
-echo h($name) . "<br />";
-
+$subject = find_subject_by_id($id);
 ?>
 
-<a href="show.php?name=<?php echo u('John Doe'); ?>">Links</a><br />
-<a href="show.php?name=<?php echo u('Widgets&More'); ?>">Links</a><br />
-<a href="show.php?name=<?php echo u('!#*?'); ?>">Links</a><br />
+<?php $page_title = 'Show Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="subject show">
+
+    <h1>Subject: <?php echo h($subject['menu_name']); ?></h1>
+
+    <div class="attributes">
+        <dl>
+            <dt>Menu Name</dt>
+            <dd><?php echo h($subject['menu_name']); ?></dd>
+        </dl>
+        <dl>
+            <dt>Position</dt>
+            <dd><?php echo h($subject['position']); ?></dd>
+        </dl>
+        <dl>
+            <dt>Visible</dt>
+            <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+        </dl>
+
+    </dl>
+    </div>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
