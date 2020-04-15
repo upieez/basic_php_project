66651c27f5046a5eb14cf2134d3e5760e7d38158
upieez <samuelhww@gmail.com>
2020-04-15 16:27:54 +0800

    add code to edit page

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -13,10 +13,25 @@ $visible = '';
 $content = '';
 
 if(is_post_request()){
-    $menu_name = $_POST['menu_name'] ?? '';
-    $position = $_POST['position'] ?? '';
-    $visible = ($_POST['visible'] == 1 ) ? 'visible' : 'not visible';
-    $content = $_POST['content'] ?? '';
+
+    $page = [];
+    $page['id'] = $id;
+    $page['subject_id'] = $_POST['subject_id'] ?? '';
+    $page['menu_name'] = $_POST['menu_name'] ?? '';
+    $page['position'] = $_POST['position'] ?? '';
+    $page['visible'] = $_POST['visible'] ?? '';
+    $page['content'] = $_POST['content'] ?? '';
+
+    $result = update_page($page);
+    redirect_to(url_for('/staff/pages/show.php?id=' . $id));
+
+} else {
+
+  $page = find_page_by_id($id);
+
+  $page_set = find_all_pages();
+  $page_count = mysqli_num_rows($page_set);
+  mysqli_free_result($page_set);
 
 }
 ?>
@@ -34,13 +49,40 @@ if(is_post_request()){
     <form action="<?php echo url_for('/staff/pages/edit.php?id=' . $id)?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="<?php echo $menu_name ?>" /></dd>
+        <dd><input type="text" name="menu_name" value="<?php echo h($page['menu_name']); ?>" /></dd>
+      </dl>
+      <dl>
+      <dl>
+        <dt>Subject</dt>
+        <dd>
+        <select name="subject_id">
+        <?php
+          $subject_set = find_all_subjects();
+          while($subject = mysqli_fetch_assoc($subject_set)){
+            echo "<option value=\"" . h($subject['id']) . "\"";
+            if($page["subject_id"] == $subject['id']){
+              echo " selected";
+            }
+            echo ">" . h($subject['menu_name']) . "</option>";
+          }
+          mysqli_free_result($subject_set);
+        ?>
+        </select>
+        </dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1">1</option>
+            <?php
+              for($i=0;$i<=$page_count;$i++){
+                echo "<option value=\"{$i}\"";
+                if($page["position"] == $i){
+                  echo " selected";
+              }
+              echo ">{$i}</option>";
+            }
+            ?>
           </select>
         </dd>
       </dl>
@@ -54,7 +96,7 @@ if(is_post_request()){
       <dl>
         <dt>Content</dt>
         <dd>
-          <textarea name="content" rows="5" column="20"><?php echo $content; ?></textarea>
+          <textarea name="content" rows="5" column="20"><?php echo $page['content']; ?></textarea>
         </dd>
       </dl>
       <div id="operations">
