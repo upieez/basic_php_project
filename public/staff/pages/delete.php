07f50d9443e663742edda5a82eb14c5058c2a7b6
upieez <samuelhww@gmail.com>
2020-04-15 16:27:46 +0800

    add delete for pages

---
+++ public/staff/pages/delete.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('../../../private/initialize.php');
+
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/staff/pages/index.php'));
+}
+
+$id = $_GET['id'];
+
+if(is_post_request()) {
+
+    $result = delete_page($id);
+    redirect_to(url_for('/staff/pages/index.php'));
+
+} else {
+    $page = find_page_by_id($id);
+}
+
+?>
+
+<?php $page_title = 'Delete Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="pages delete">
+    <h1>Delete Pages</h1>
+    <p>Are you sure you want to delete this subject?</p>
+    <p class="item"><?php echo h($page['menu_name']); ?></p>
+
+    <form action="<?php echo url_for('/staff/pages/delete.php?id=' . h(u($page['id']))); ?>" method="post">
+      <div id="operations">
+        <input type="submit" name="commit" value="Delete Subject" />
+      </div>
+    </form>
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
