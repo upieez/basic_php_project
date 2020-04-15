07d790dec19517afa5f755be7b67b843c9ee19d3
upieez <samuelhww@gmail.com>
2020-04-15 20:12:49 +0800

    add mysqli escape string to dynamic data

--- private/query_functions.php
+++ private/query_functions.php
@@ -15,7 +15,7 @@
         global $db;
 
         $sql = "SELECT * FROM subjects ";
-        $sql .= "WHERE id='" . $id . "'";
+        $sql .= "WHERE id='" . db_escape($db,$id) . "'";
         $result = mysqli_query($db, $sql);
         confirm_result_set($result);
         $subject = mysqli_fetch_assoc($result); 
@@ -141,9 +141,8 @@
         $sql =  "INSERT INTO subjects ";
         $sql .= "(menu_name, position, visible) ";
         $sql .= "VALUES (";
-        $sql .= "'" . $subject['menu_name'] . "',";
-        $sql .= "'". $subject['position'] . "',";
-        $sql .= "'" . $subject['visible'] . "'";
+        $sql .= "'" . db_escape($db,$subject['menu_name']) . "',";
+        $sql .= "'" . db_escape($db,$subject['visible']) . "'";
         $sql .= ")";
         $result = mysqli_query($db,$sql);
         // For INSERT statements, $result is true/false
@@ -214,11 +213,11 @@
         $sql = "INSERT INTO pages ";
         $sql .= "(subject_id, menu_name, position, visible, content) ";
         $sql .= "VALUES (";
-        $sql .= "'" . $page['subject_id'] . "', ";
-        $sql .= "'" . $page['menu_name'] . "', "; 
-        $sql .= "'" . $page['position'] . "', "; 
-        $sql .= "'" . $page['visible'] . "', "; 
-        $sql .= "'" . $page['content'] . "'";
+        $sql .= "'" . db_escape($db,$page['subject_id']) . "', ";
+        $sql .= "'" . db_escape($db,$page['menu_name']) . "', "; 
+        $sql .= "'" . db_escape($db,$page['position']) . "', "; 
+        $sql .= "'" . db_escape($db,$page['visible']) . "', "; 
+        $sql .= "'" . db_escape($db,$page['content']) . "'";
         $sql .= ")"; 
 
         $result = mysqli_query($db,$sql);
@@ -243,12 +242,12 @@
         }
 
         $sql = "UPDATE pages SET ";
-        $sql .= "subject_id='" . $page['subject_id'] . "', ";
-        $sql .= "menu_name='" . $page['menu_name'] . "', ";
-        $sql .= "position='" . $page['position'] . "', ";
-        $sql .= "visible='" . $page['visible'] . "', ";
-        $sql .= "content='" . $page['content'] . "' ";
-        $sql .= "WHERE id='" . $page['id'] . "' ";
+        $sql .= "subject_id='" . db_escape($db,$page['subject_id']) . "', ";
+        $sql .= "menu_name='" . db_escape($db,$page['menu_name']) . "', ";
+        $sql .= "position='" . db_escape($db,$page['position']) . "', ";
+        $sql .= "visible='" . db_escape($db,$page['visible']) . "', ";
+        $sql .= "content='" . db_escape($db,$page['content']) . "' ";
+        $sql .= "WHERE id='" . db_escape($db,$page['id'] ). "' ";
         $sql .= "LIMIT 1";
 
         $result = mysqli_query($db, $sql);
--- private/validation_functions.php
+++ private/validation_functions.php
@@ -102,14 +102,18 @@
     global $db;
 
     $sql = "SELECT * FROM pages ";
-    $sql .= "WHERE menu_name='" . $menu_name . "' ";
-    $sql .= "AND id != '" . $current_id . "'";
+    $sql .= "WHERE menu_name='" . db_escape($db,$menu_name) . "' ";
+    $sql .= "AND id != '" . db_escape($db,$current_id) . "'";
 
     $page_set = mysqli_query($db, $sql);
     $page_count = mysqli_num_rows($page_set);
     mysqli_free_result($page_set);
 
     return $page_count === 0;
+}
+
+  function db_escape($connection,$string){
+    return mysqli_real_escape_string($connection, $string);
 }
 
 ?>
