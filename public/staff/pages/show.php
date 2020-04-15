58e075b98cac2e29a68dbadd1ab293af0ba36bbf
upieez <samuelhww@gmail.com>
2020-04-15 13:46:27 +0800

    add formatting for page show

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -12,10 +12,35 @@
     <div><a href="<?php echo url_for('/staff/pages/index.php'); ?>
     ">&laquo; Back to list</a></div>
 
-    <?php
-    echo "ID is: " . h($id) . "<br />";
-    echo "Content: " . h($page['content']) . "<br />";
-    ?>
+    <div class="page show">
+    <h1>Page: <?php echo h($page['menu_name']); ?></h1>
+
+    <div class="attributes">
+        <dl>
+            <dt>Menu Name</dt>
+            <dd><?php echo h($page['menu_name']);?></dd>
+        </dl>
+        <dl>
+            <dt>Subject</dt>
+            <dd><?php echo h($page['subject_id']);?></dd>
+        </dl>
+        <dl>
+            <dt>Position</dt>
+            <dd><?php echo h($page['position']);?></dd>
+        </dl>
+        <dl>
+            <dt>Visible</dt>
+            <dd><?php echo h($page['visible']);?></dd>
+        </dl>
+        <dl>
+            <dt>Content</dt>
+            <dd><?php echo h($page['content']);?></dd>
+        </dl>
+    
+    </div>
+    
+  </div>
+
 </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php');
